716d678af88fa7e28704cf35ee026d3434041a4f
Bingjun Zhang <bingjunzhang@gmail.com>
2018-08-26 16:36:27 -0700

    Add method to get the masked word

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 // TODO: capitalize the letters obtained from dict API
-const undiscoveredLetters = ["W", "O", "R", "D"];
+let word = "CORRECT";
+const undiscoveredLetters = ["C", "O", "R", "E", "T"];
 const discoveredLetters = [];
 const wrongGuesses = [];
+// gameResult: undefined - in game; true - win; false - lose
 let gameResult = undefined;
 
 const elements = document.getElementsByClassName("letter");
@@ -9,15 +11,16 @@ for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
     element.addEventListener("click", function () {
         const typedLetter = element.textContent; 
-        if (discoveredLetters.indexOf(typedLetter) !== -1 || 
-            wrongGuesses.indexOf(typedLetter) !== -1 || 
-            gameResult === true ||
-            gameResult === false) {
-            return;
+        if (discoveredLetters.indexOf(typedLetter) !== -1 || // check if typedLetter in discoveredLetters array
+            wrongGuesses.indexOf(typedLetter) !== -1 || // check if typedLetter in wrongGuesses array
+            gameResult === true || // check if already win
+            gameResult === false) { // check if already lose
+            return; // return if the letter has been clicked or the game ended
         }
         const index = undiscoveredLetters.indexOf(typedLetter);
         if (index === -1) {
             wrongGuesses.push(typedLetter);
+            element.classList.add("wrong");
             console.log(wrongGuesses);
             if (wrongGuesses.length === 6) {
                 // TODO: wire to UI
@@ -28,6 +31,14 @@ for (let i = 0; i < elements.length; i++) {
             undiscoveredLetters.splice(index, 1);
             console.log(undiscoveredLetters);
             discoveredLetters.push(typedLetter);
+            element.classList.add("correct");
+            for (let i = 0; i < undiscoveredLetters.length; i++) {
+                let letter = undiscoveredLetters[i];
+                // console.log(letter);
+                word = word.replace(/letter/g, "_"); // use regex to replace all occurance of letter
+                console.log(word);
+            }
+            
             console.log(discoveredLetters);
             if (undiscoveredLetters.length === 0) {
                 gameResult = true;
