05a127a6eedc6d28a90a9a09ba34b75c12b6aa5f
Bingjun Zhang <bingjunzhang@gmail.com>
2018-08-27 00:02:16 -0700

    Fetch words from dict API

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,63 +1,71 @@
 // TODO: fetch random word from dict API
-http://app.linkedin-reach.io/words
-// TODO: capitalize letters
-let word = "CORRECT";
-// TODO: convert the word to array of unique letters
-const undiscoveredLetters = ["C", "O", "R", "E", "T"];
-const discoveredLetters = [];
-const wrongGuesses = [];
-// gameStatus: undefined - in game; true - win; false - game over
-let gameStatus = undefined;
-
-displayMaskedWord(word, undiscoveredLetters);
-
-const elements = document.getElementsByClassName("letter");
-// listen to click event for each letter
-for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-    const letter = element.textContent; 
-    element.addEventListener("click", function () {
-
-        if (discoveredLetters.indexOf(letter) !== -1 || // check if letter is discovered
-            wrongGuesses.indexOf(letter) !== -1 || // check if letter has been wrongly guessed
-            gameStatus !== undefined) { // check if game ended
-            return; // skip click event handling
-        }
-
-        const index = undiscoveredLetters.indexOf(letter);
-
-        if (index === -1) { // if the guess was wrong
-            wrongGuesses.push(letter); // mark the letter as a wrong guess
-            element.classList.add("wrong");
-            // update the number of remaining guesses
-            document.getElementById("remainingGuesses").textContent = 6 - wrongGuesses.length;
-            if (wrongGuesses.length === 6) {
-                gameStatus = false; // game over
-                document.getElementById("hangman").textContent = "Game over!";
+let words = [];
+let request = new XMLHttpRequest();
+const url = "http://app.linkedin-reach.io/words";
+request.open("GET", url, true);
+request.onload = function () {
+    words = request.response.split("\n");
+    // TODO: get random word from words[]
+    const word = words[0].toUpperCase();
+    console.log(word);
+    // TODO: convert the word to array of unique letters
+    const undiscoveredLetters = ["C", "O", "R", "E", "T"];
+    const discoveredLetters = [];
+    const wrongGuesses = [];
+    // gameStatus: undefined - in game; true - win; false - game over
+    let gameStatus = undefined;
+    
+    displayMaskedWord(word, undiscoveredLetters);
+    
+    const elements = document.getElementsByClassName("letter");
+    // listen to click event for each letter
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        const letter = element.textContent; 
+        element.addEventListener("click", function () {
+    
+            if (discoveredLetters.indexOf(letter) !== -1 || // check if letter is discovered
+                wrongGuesses.indexOf(letter) !== -1 || // check if letter has been wrongly guessed
+                gameStatus !== undefined) { // check if game ended
+                return; // skip click event handling
             }
-        }
-        
-        else { // if the guess was correct
-            // mark letter as discovered
-            undiscoveredLetters.splice(index, 1);
-            discoveredLetters.push(letter);
-
-            element.classList.add("correct");
-            displayMaskedWord(word, undiscoveredLetters);
-            if (undiscoveredLetters.length === 0) { // if all letters were discovered               
-                gameStatus = true; // you win                
-                document.getElementById("hangman").textContent = "You win!";
+    
+            const index = undiscoveredLetters.indexOf(letter);
+    
+            if (index === -1) { // if the guess was wrong
+                wrongGuesses.push(letter); // mark the letter as a wrong guess
+                element.classList.add("wrong");
+                // update the number of remaining guesses
+                document.getElementById("remainingGuesses").textContent = 6 - wrongGuesses.length;
+                if (wrongGuesses.length === 6) {
+                    gameStatus = false; // game over
+                    document.getElementById("hangman").textContent = "Game over!";
+                }
             }
-        }
-    });
-}
-
-// generate masked word and display it
-function displayMaskedWord(word, undiscoveredLetters) {
-    for (let i = 0; i < undiscoveredLetters.length; i++) {
-        let letter = undiscoveredLetters[i];
-        // mask all undiscovered letters
-        word = word.replace(new RegExp(letter, "g"), "_"); 
+            
+            else { // if the guess was correct
+                // mark letter as discovered
+                undiscoveredLetters.splice(index, 1);
+                discoveredLetters.push(letter);
+    
+                element.classList.add("correct");
+                displayMaskedWord(word, undiscoveredLetters);
+                if (undiscoveredLetters.length === 0) { // if all letters were discovered               
+                    gameStatus = true; // you win                
+                    document.getElementById("hangman").textContent = "You win!";
+                }
+            }
+        });
     }
-    document.getElementById("currentWord").textContent = word;
+    
+    // generate masked word and display it
+    function displayMaskedWord(word, undiscoveredLetters) {
+        for (let i = 0; i < undiscoveredLetters.length; i++) {
+            let letter = undiscoveredLetters[i];
+            // mask all undiscovered letters
+            word = word.replace(new RegExp(letter, "g"), "_"); 
+        }
+        document.getElementById("currentWord").textContent = word;
+    }
 }
+request.send();
