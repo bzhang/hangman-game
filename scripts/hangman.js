2b2ba73b0b28eed18e0a7c68f398e255d664500d
Bingjun Zhang <bingjunzhang@gmail.com>
2018-08-28 22:48:25 -0700

    Add modal actions to open and close leader board

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -20,7 +20,8 @@ let playerName = "";
 let points = 0;
 
 let request = new XMLHttpRequest();
-const url = "https://cors-anywhere.herokuapp.com/http://app.linkedin-reach.io/words";
+// const url = "https://cors-anywhere.herokuapp.com/http://app.linkedin-reach.io/words";
+const url = "http://app.linkedin-reach.io/words"; // use this URL if already installed CORS Chrome extention
 request.open("GET", url, true);
 request.onload = function () {
     words = request.response.split("\n");
@@ -159,7 +160,7 @@ function checkIfLost() {
             heartElement.style.width = nHeart * 32 + "px";
         } else {
             // TODO: pop out lightbox to record player name and points
-            
+
         }
     }
 }
@@ -217,3 +218,24 @@ onload = function startAnimation() {
         div.style.backgroundPosition = "0px " + frameOffset + "px"; 
     }, 100); 
 } 
+
+// Get the modal
+let modal = document.getElementById("leaderBoardModal");
+// Get the button that opens the modal
+let btn = document.getElementById("leaderBoardBtn");
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
